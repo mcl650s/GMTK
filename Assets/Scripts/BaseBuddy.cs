2b0ccb00f64331b5b260471bb32b108606101556
Jakob <horvath.j17@gmail.com>
2021-06-11 17:47:56 -0600

    buddy starter scripts

---
+++ Assets/Scripts/BaseBuddy.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseBuddy : MonoBehaviour
+{
+    public abstract void HoldingHandsState();
+    public abstract void AloneState();
+}
---
+++ Assets/Scripts/FatassBuddy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FatassBuddy : BaseBuddy
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {   
+    }
+
+    public override void HoldingHandsState()
+    {
+
+    }
+
+    public override void AloneState() 
+    {
+
+    }
+}
