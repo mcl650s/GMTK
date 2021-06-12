47de7c594c3780f578a170b2e2778dc4eae85b8d
Jakob <horvath.j17@gmail.com>
2021-06-12 14:22:24 -0600

    better falling block logic

--- Assets/Scripts/FallingBlock.cs
+++ Assets/Scripts/FallingBlock.cs
@@ -7,29 +7,36 @@ public class FallingBlock : MonoBehaviour
     public float fallSpeed = 8.0f;
     private GameManager manager;
 
-    void Start() 
+    void Start()
     {
         manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
     }
 
-    private void OnCollisionEnter2D(Collision2D col) 
+    private void OnCollisionEnter2D(Collision2D col)
     {
-        if (manager.currentBuddy.Equals("Fatass") && manager.player.transform.position.y > transform.position.y)
+        if (manager.currentBuddy.Equals("Fatass"))
         {
-            StartCoroutine(ShakeAndFall());
+            Vector3 contactPoint = col.contacts[0].point;
+            Vector3 center = transform.position;
+            bool top = contactPoint.y > center.y;
+
+            if (top)
+            {
+                StartCoroutine(ShakeAndFall());
+            }
         }
     }
 
-    private IEnumerator ShakeAndFall() 
+    private IEnumerator ShakeAndFall()
     {
         bool shakingDown = true;
 
-        for (int i = 0; i < 100; i++) 
+        for (int i = 0; i < 100; i++)
         {
             gameObject.transform.position = shakingDown ? gameObject.transform.position - new Vector3(0, 0.005f, 0) :
                                                           gameObject.transform.position + new Vector3(0, 0.005f, 0);
 
-            if (i % 10 == 0) 
+            if (i % 10 == 0)
             {
                 shakingDown = !shakingDown;
             }
